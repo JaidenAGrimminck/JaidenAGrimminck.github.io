9ee62bc291c763d96f5ab45a506a32f0f8f5bbf0
JaidenAGrimminck <jaidng1@gmail.com>
2025-09-25 00:11:00 -0400

    edits?

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { //hosted on JaidenAGrimminck.github.io so hosted at root
+    output: 'export', // This enables static export
+    basePath: process.env.NODE_ENV === 'production' ? '' : '', // Adjust for GitHub Pages URL structure
+    assetPrefix: process.env.NODE_ENV === 'production' ? '' : '', // Adjust for GitHub Pages URL structure
+};
 
 export default nextConfig;
